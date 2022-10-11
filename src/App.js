51efa9cbaaa1955b340f146ccfbd69755608a131
shayan jafari <shayanja12345@gmail.com>
2022-10-11 16:59:03 +0330

    useRef example 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import UseRefExample2 from "./components/UseRefExample2";
+import UseRefExample3 from "./components/UseRefExample3";
 
 const App = () => {
   return (
     <div className="container mt-5">
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 };
